ea6f44eb8d4b8469f1e0246dd4d642dd50082e66
Ben Makusha <btmakusha@gmail.com>
2017-08-29 20:50:44 -0400

    Added Firebase configs to project

--- hitchhkr-dev/AppDelegate.swift
+++ hitchhkr-dev/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window?.rootViewController = containerVC
         window?.makeKeyAndVisible()
+        
+        FIRApp.configure()
         return true
     }
 
