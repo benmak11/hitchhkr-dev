ce6231d211dd6a9181eaa89969538528d94e29d9
Ben Makusha <btmakusha@gmail.com>
2017-09-04 14:09:04 -0400

    Implemented UITextFieldDelegate

--- hitchhkr-dev/HomeVC.swift
+++ hitchhkr-dev/HomeVC.swift
@@ -17,6 +17,8 @@ class HomeVC: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var actionBtn: RoundedShadowButton!
     @IBOutlet weak var centerMapBtn: UIButton!
+    @IBOutlet weak var destinationTextField: UITextField!
+    @IBOutlet weak var destinationCircle: CircleView!
     
     var delegate: CenterVCDelegate?
     
@@ -25,6 +27,8 @@ class HomeVC: UIViewController {
     var regionRadius: CLLocationDistance = 1000
     
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
+    
+    var tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +40,7 @@ class HomeVC: UIViewController {
         checkLocationAuthStatus()
         
         mapView.delegate = self
+        destinationTextField.delegate = self
         
         centerMapOnUserLocation()
         
@@ -162,6 +167,98 @@ extension HomeVC: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
         centerMapBtn.fadeTo(alphaValue: 1.0, withDuration: 0.2)
+    }
+}
+
+extension HomeVC: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField == destinationTextField {
+            tableView.frame = CGRect(x: 20, y: view.frame.height, width: (view.frame.width - 40), height: (view.frame.height - 170))
+            tableView.layer.cornerRadius = 5.0
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "locationCell")
+            
+            tableView.delegate = self
+            tableView.dataSource = self
+            
+            tableView.tag = 18
+            tableView.rowHeight = 60
+            
+            view.addSubview(tableView)
+            animateTableView(shouldShow: true)
+            
+            UIView.animate(withDuration: 0.2, animations: { 
+                self.destinationCircle.backgroundColor = UIColor.red
+                self.destinationCircle.borderColor = UIColor.init(red: 199/250, green: 0/250, blue: 0/250, alpha: 1.0)
+            })
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == destinationTextField {
+            // performSearch
+            view.endEditing(true)
+        }
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if textField == destinationTextField {
+            if destinationTextField.text == "" {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.destinationCircle.backgroundColor = UIColor.lightGray
+                    self.destinationCircle.borderColor = UIColor.darkGray
+                })
+            }
+        }
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        centerMapOnUserLocation()
+        return true
+    }
+    
+    func animateTableView(shouldShow: Bool) {
+        
+        if shouldShow {
+            UIView.animate(withDuration: 0.2, animations: { 
+                self.tableView.frame = CGRect(x: 20, y: 170, width: (self.view.frame.width - 40), height: (self.view.frame.height - 170))
+            })
+        } else {
+            UIView.animate(withDuration: 0.2, animations: { 
+                self.tableView.frame = CGRect(x: 20, y: self.view.frame.height, width: (self.view.frame.width - 40), height: (self.view.frame.height - 170))
+            }, completion: { (finished) in
+                for subView in self.view.subviews {
+                    if subView.tag == 18 {
+                        subView.removeFromSuperview()
+                    }
+                }
+            })
+        }
+    }
+}
+
+extension HomeVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        animateTableView(shouldShow: false)
+        print("selected!")
     }
 }
 
